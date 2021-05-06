4a3c2e798e03073220316ce7b58a82d82bd36c3a
Роман Донченко <dpb@corrigendum.ru>
2021-05-07 00:41:29 +0300

    Add some basic argument parsing
    Just enough to check that we have exactly one positional argument.

--- src/main.cxx
+++ src/main.cxx
@@ -1,4 +1,29 @@
+#include <cstring>
+#include <filesystem>
+#include <iostream>
+
 int
-main() {
+main(int, char **argv) {
+    char **pp_arg = argv + 1;
+
+    if (*pp_arg) {
+        if (!std::strcmp(*pp_arg, "--")) {
+            ++pp_arg;
+        }
+        else if (**pp_arg == '-') {
+            std::cerr << "unknown option: " << *pp_arg << "\n";
+            return 1;
+        }
+    }
+
+    if (!pp_arg[0] || pp_arg[1]) {
+        std::cerr << "usage: " << argv[0] << " source.pas\n";
+        return 1;
+    }
+
+    std::filesystem::path source_path(pp_arg[0]);
+
+    std::cout << "source path: " << source_path << std::endl;
+
     return 0;
 }
